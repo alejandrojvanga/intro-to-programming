a45ce097d827d854e7d305f8541339dff85514b9
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-05 16:32:40 -0500

    Basic Bank structure

---
+++ src/week1/BankingSolution/Bank/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace Bank;
+
+public class BankAccount
+{
+    private decimal _currentBalance = 5000M;
+    public void Deposit(decimal amountToDeposit)
+    {
+        _currentBalance += amountToDeposit;
+    }
+
+    public decimal GetBalance()
+    {
+        return _currentBalance;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        _currentBalance -= amountToWithdraw;
+    }
+}
---
+++ src/week1/BankingSolution/Banking.UnitTests/Account/MakingDeposits.cs
@@ -0,0 +1,22 @@
+﻿using Bank;
+
+namespace Banking.UnitTests.Accounts;
+public class MakingDeposits
+{
+    [Theory]
+    [InlineData(100)]
+    [InlineData(432.2)]
+    public void MakingADespositIncreasesBalance(decimal amountToDeposit)
+    {
+        // given
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        // when
+        // WTCYWYH
+        account.Deposit(amountToDeposit);
+
+        // then
+        Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+    }
+}
