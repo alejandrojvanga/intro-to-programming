768b169d4bfdabd37c33ad6417d3811d8a3c4e59
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-06 08:50:18 -0500

    Notes of 3/5/2024

--- src/week1/BankingSolution/Banking.UnitTests/Account/MakingWithdrawls.cs
+++ src/week1/BankingSolution/Banking.UnitTests/Account/MakingWithdrawls.cs
@@ -25,4 +25,24 @@ public class MakingWithdrawls
         Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
 
     }
+    [Fact]
+    public void OverDraftNotAllowed()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        account.Withdraw(account.GetBalance() + .01M);
+
+        Assert.Equal(openingBalance, account.GetBalance());
+    }
+
+    [Fact]
+    public void AreAnyDecimalsAllowed()
+    {
+        var account = new BankAccount();
+
+        account.Withdraw(-5424);
+
+        Assert.Equal(5000M, account.GetBalance());
+    }
 }
