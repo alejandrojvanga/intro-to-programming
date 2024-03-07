26c447c0f3057ad17d72a1cd1f6bcfa1ed64c67e
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-07 10:07:41 -0500

    before branch

--- src/week1/BankingSolution/Banking.UnitTests/Account/MakingWithdrawls.cs
+++ src/week1/BankingSolution/Banking.UnitTests/Account/MakingWithdrawls.cs
@@ -39,16 +39,6 @@ public class MakingWithdrawls
         Assert.Equal(openingBalance, account.GetBalance());
     }
 
-    [Fact]
-    public void CanWithdrawAllMoney()
-    {
-        var account = new BankAccount();
-
-        account.Withdraw(account.GetBalance());
-
-        Assert.Equal(0, account.GetBalance());
-    }
-
     [Theory]
     [InlineData(-1.0)]
     [InlineData(0)]
