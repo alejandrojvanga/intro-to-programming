3d976927b31cc21aeddff9321a3fcf681ad33161
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-11 15:38:13 -0400

    expanding on angular

--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { DemoComponent } from './components/demo/demo.component';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <app-page-header />
-    <main>
-      <app-demo />
-      <app-demo />
-      <app-demo />
-    </main>
+    <div class="container mx-auto">
+      <app-page-header />
+      <nav>
+        <app-nav-bar />
+        <main>
+          <router-outlet />
+        </main>
+      </nav>
+    </div>
   `,
   styles: [],
-  imports: [PageHeaderComponent, DemoComponent],
+  imports: [PageHeaderComponent, RouterOutlet, NavBarComponent],
 })
 export class AppComponent {}
---
+++ src/week2/app/frontend/src/app/pages/todo-list/components/todo-item-list/todo-item-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item-list',
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: ``,
+})
+export class TodoItemListComponent {}
