ec7699bf522b837555ea91203ad9be9d3b7fcca8
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-11 14:49:52 -0400

    counter

--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -1,28 +1,19 @@
 import { Component } from '@angular/core';
-import { PageHeaderComponent } from "./components/page-header/page-header.component";
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    template: `
- <app-page-header/>
- <main>
-    <p>Our Stuff Goes Here</p>
-
-    <div role="alert" class="alert alert-warning">
-  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-  <span>Warning: Invalid email address!</span>
-
-  <button> Click Me!</button>
-  <button class="btn btn-info">Info</button>
-  
-</div>
-
- </main>
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <app-page-header />
+    <main>
+      <app-demo />
+      <app-demo />
+      <app-demo />
+    </main>
   `,
-    styles: [],
-    imports: [PageHeaderComponent]
+  styles: [],
+  imports: [PageHeaderComponent, DemoComponent],
 })
-
-export class AppComponent {
-}
+export class AppComponent {}
---
+++ src/week2/app/frontend/src/app/components/demo/demo.component.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      {{ counter() }}
+    </p>
+    <button (click)="doIt()" class="btn btn-primary">
+      Change the text above
+    </button>
+  `,
+  styles: ``,
+})
+export class DemoComponent {
+  counter = signal(0);
+
+  doIt() {
+    this.counter.set(this.counter() + 1);
+  }
+}
