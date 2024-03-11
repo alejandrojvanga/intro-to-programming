42a6d7a31f5fbdb432db3e0653b7a16a9d7fa898
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-11 12:09:56 -0400

    Building our frontend (buttons, syles, and header)

--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PageHeaderComponent } from "./components/page-header/page-header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+    selector: 'app-root',
+    standalone: true,
+    template: `
+ <app-page-header/>
+ <main>
+    <p>Our Stuff Goes Here</p>
 
-    <router-outlet />
+    <div role="alert" class="alert alert-warning">
+  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+  <span>Warning: Invalid email address!</span>
+
+  <button> Click Me!</button>
+  <button class="btn btn-info">Info</button>
+  
+</div>
+
+ </main>
   `,
-  styles: [],
+    styles: [],
+    imports: [PageHeaderComponent]
 })
+
 export class AppComponent {
-  title = 'frontend';
 }
