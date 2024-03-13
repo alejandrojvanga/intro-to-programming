1ac5121b6e41c019ff470a49454b373d5c64c975
alejandrojvanga <alejandro_j_vanga_guzman@progressive.com>
2024-03-13 12:11:21 -0400

    Bff.Api

---
+++ src/week2/app/BffSolution/Bff.Api/Status/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bff.Api.Status;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public IActionResult GetTheStatus()
+    {
+        return Ok("~Everyhting is groovy");
+    }
+}
